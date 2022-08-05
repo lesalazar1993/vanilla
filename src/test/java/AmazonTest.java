a9a33c9892829e23785ea5f7aa0b3828b3f44fc9
lesalazar1993 <Lesalazar93@hotmail.es>
2022-08-04 22:52:10 -0600

    Add tearDown method to quit the driver at the end

--- src/test/java/AmazonTest.java
+++ src/test/java/AmazonTest.java
@@ -4,8 +4,10 @@ import amazon.pageObjects.DepartmentPage;
 import amazon.pageObjects.HomePage;
 import amazon.pageObjects.ProductPage;
 import com.typesafe.config.Config;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -15,10 +17,12 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AmazonTest {
     private static Config config = EnvFactory.getInstance().getConfig();
     private static final String HOME_PAGE_URL = config.getString("HOME_PAGE_URL");
     private WebDriver driver = DriverFactory.getDriver();
+
 
     @DisplayName("Should be able to find 'About this Item' title exists")
     @Test
@@ -45,5 +49,10 @@ public class AmazonTest {
 
         ProductPage productPage = new ProductPage(driver);
         assertEquals("About this item", productPage.getElementText(By.cssSelector("#feature-bullets > h1.a-text-bold")));
+    }
+
+    @AfterAll
+    void tearDown() {
+        driver.quit();
     }
 }
